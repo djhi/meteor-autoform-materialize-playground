a83a51a66e3f52c5094a888779d8d95e993e2d25
djhi <gildas.garcia@gmail.com>
2015-03-26 23:46:17 +0100

    1st commit

---
+++ autoform-materialize-tests.js
@@ -0,0 +1,110 @@
+Schemas = {}
+
+Schemas.Address = new SimpleSchema({
+  street: {
+    type: String
+  },
+  street2: {
+    type: String,
+    optional: true
+  },
+  city: {
+    type: String
+  },
+  state: {
+    type: String,
+    allowedValues: ["AL","AK"],
+    autoform: {
+      afFieldInput: {
+        type: 'select',
+        firstOption: "(Select a State)"
+      }
+    }
+  },
+  postalCode: {
+    type: String,
+    label: "ZIP"
+  },
+  isPrimaryAddress: {
+    type: Boolean
+  }
+});
+
+Schemas.Contact = new SimpleSchema({
+  name: {
+    type: String
+  },
+  title: {
+    type: String,
+    allowedValues: ['Miss', 'Mrs', 'Mr'],
+    autoform: {
+      options: 'allowed'
+    }
+  },  
+  phone: {
+    type: String
+  }
+});
+Schemas.FieldsExamples = new SimpleSchema({
+  name: {
+    type: String
+  },
+  phone: {
+    type: String,
+    optional: true
+  },
+  dateOfBirth: {
+    type: Date,
+    autoform: {
+      type: 'pickadate'
+    }
+  },
+  title: {
+    type: String,
+    allowedValues: ['Miss', 'Mrs', 'Mr'],
+    autoform: {
+      options: 'allowed'
+    }
+  },
+  address: {
+    type: Schemas.Address
+  },
+  contacts: {
+    type: [Schemas.Contact],
+    optional: true
+  }
+});
+
+Contacts = new Mongo.Collection('contacts');
+Contacts.attachSchema(Schemas.Contact);
+
+if (Meteor.isClient) {
+
+  AutoForm.setDefaultTemplate('materialize');
+
+  Template.registerHelper('Schemas', function() {
+    return Schemas;
+  });
+
+  AutoForm.addHooks('testForm',  {
+    formToDoc: function(doc) {
+      console.log(doc);
+      return doc;
+    },
+    onError: function(operation, error) {
+      if(error){
+        console.log(error);
+        alert('Dang, error: ' + error.message);
+      }
+    },
+    onSuccess: function(){
+      alert('all good baby !');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
